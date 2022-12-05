721ed723b197572313c69a26255d882ca93bc085
lioGR <liorgrishinnew@gmail.com>
2022-12-05 15:11:57 +0200

    added logic if user is in cookies

--- Startup.v1/client/src/App.tsx
+++ Startup.v1/client/src/App.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
 import { Screen } from "./components/screen/Screen.styled";
 import { Container } from "./components/container/Container.styled";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { Login } from "./views/login/Login";
 import { Register } from './views/register/Register';
-import UserAgreement from "./views/userAgreement/UserAgreement";
 import Intro from "./views/intro/Intro";
 import { UserInfo } from './views/userInformation/UserInfo';
 import Home from "./views/home/Home";
 import Favorites from "./views/favorites/Favorites";
 import List from "./views/list/List";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { useEffect } from "react";
+import { getUserByCookie } from "./features/user/userAPI";
+import { userSelector } from "./features/user/userSlice";
 
 function App() {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getUserByCookie());
+  }, []);
+  
   return (
     <Container>
       <Screen>
--- Startup.v1/server/API/users/usersCtrl.ts
+++ Startup.v1/server/API/users/usersCtrl.ts
@@ -47,22 +47,26 @@ export function getUserByCookie(req: express.Request, res: express.Response) {
         if (!secret) throw new Error("Couldn't load secret from .env");
 
         const { userID } = req.cookies;
-        if (!userID) throw new Error("Couldn't find cookie named userID in application");
+        // if (!userID) throw new Error("Couldn't find cookie named userID in application");
 
         const decodeUserId = jwt.decode(userID, secret);
-        const { userId } = decodeUserId;
-        if (!userId) throw new Error("Couldn't find userId from decodedUserId");
-
-        const sql = `SELECT * FROM users WHERE user_id = '${userId}'`;
-
-        connection.query(sql, (error, result) => {
-            try {
-                if (error) throw error;
-                res.send({ result });
-            } catch (error) {
-                res.status(500).send({ error: error.message });
-            }
-        });
+        if(userID && decodeUserId) {
+            const { userID } = decodeUserId;
+            if (!userID) throw new Error("Couldn't find userId from decodedUserId");
+    
+            const sql = `SELECT * FROM users WHERE user_id = '${userID}'`;
+    
+            connection.query(sql, (error, result) => {
+                try {
+                    if (error) throw error;
+                    res.send({ result });
+                } catch (error) {
+                    res.status(500).send({ error: error.message });
+                }
+            });
+        } else {
+            res.status(500).send({error: "No userID from cookies"});
+        }
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
