80d03a4b3686f6dfd939f5264e0b15198ed878e6
lioGR <liorgrishinnew@gmail.com>
2022-12-05 09:34:02 +0200

    Added logic to navigate to pages login and register from intro

--- Startup.v1/client/src/views/intro/Intro.tsx
+++ Startup.v1/client/src/views/intro/Intro.tsx
@@ -2,8 +2,10 @@ import sugarbitHeader from "../../assets/images/logo/sugarbitHeader.png";
 import professionalHeader from "../../assets/images/logo/professionalHeader.png";
 import logoP1 from "../../assets/images/logo/logoP1.png";
 import logoP2 from "../../assets/images/logo/logoP2.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const Intro = () => {
+    const navigate = useNavigate();
     return (
         <div className="intro">
             <div className="intro__header">
@@ -13,14 +15,12 @@ const Intro = () => {
                 </div>
             </div>
             <div className="intro__logo">
-                {/* <div className="intro__logo"> */}
                 <img src={logoP2} alt="logo-part-2" />
                 <img src={logoP1} alt="logo-part-1" />
-                {/* </div> */}
             </div>
             <div className="intro__container">
-                <button className="btn-main">התחברות</button>
-                <button className="btn-main">הרשמה</button>
+                <button className="btn-main" onClick={() => navigate("/login")}>התחברות</button>
+                <button className="btn-main" onClick={() => navigate("/register")}>הרשמה</button>
             </div>
         </div>
     );
