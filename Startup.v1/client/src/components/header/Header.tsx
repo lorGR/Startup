24ea18508e133364f33b1b8ff03db02feb6e4c1e
lioGR <liorgrishinnew@gmail.com>
2022-12-05 16:22:00 +0200

    Added basic logic

--- Startup.v1/client/src/components/header/Header.tsx
+++ Startup.v1/client/src/components/header/Header.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 
 interface HeaderProps {
-    headerType: string
+    headerType: string,
+    addMealForm?: boolean,
+    setAddMealForm?: Function
 }
 
-const Header: React.FC<HeaderProps> = ({ headerType }) => {
+const Header: React.FC<HeaderProps> = ({ headerType, addMealForm, setAddMealForm }) => {
+
+    const handleAddMealForm = () => {
+        try {
+            setAddMealForm!(!addMealForm);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div className="header">
-            {headerType === "addVals" && <div>הזן ערכים</div>}
-            {headerType === "carbsDisplay" && <div>פחמימות</div>}  
+            {headerType === "addVals" && <div onClick={handleAddMealForm}>הזן ערכים</div>}
+            {headerType === "carbsDisplay" && <div>פחמימות</div>}
         </div>
     )
 }
--- Startup.v1/client/src/views/home/Home.tsx
+++ Startup.v1/client/src/views/home/Home.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 
 const Home = () => {
+
+  const [addMealForm, setAddMealForm] = useState(false);
+
   return (
     <div className="home">
-        <Header headerType="addVals"/>
-        <Navbar navbarType="main"/>
+      <Header headerType="addVals" addMealForm={addMealForm} setAddMealForm={setAddMealForm} />
+      <Navbar navbarType="main" />
+      {addMealForm &&
+        <div>
+          addMealForm
+        </div>
+      }
     </div>
   )
 }
