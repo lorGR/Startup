ad5a4273adcbc13de5ee7d82dfcc09902f139bc8
Gili <gilimena95@gmail.com>
2022-12-05 14:27:19 +0200

    WIP

--- Startup.v1/client/src/views/userInformation/UserInfo.tsx
+++ Startup.v1/client/src/views/userInformation/UserInfo.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export const UserInfo = () => {
   const [date, setDate] = useState<Date>();
   const [age, setAge] = useState<number>();
+
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => {
+    const formData = JSON.stringify(data);
+    console.log(formData)
+  };
 
   function getAge(dateString: Date) {
     var today = new Date();
@@ -54,18 +61,18 @@ export const UserInfo = () => {
     }
   }
 
-  async function handleUpdateUser(event:any) {
+  async function handleUpdateUser(event: any) {
     try {
-        event.preventDefault();
-        
+      event.preventDefault();
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+        {...register("birthday")}
           onChange={handleUpdate}
           name="birthday"
           onBlur={handleChangetotext}
@@ -73,43 +80,127 @@ export const UserInfo = () => {
           type="text"
           placeholder="גיל"
         />
-        <input type="number" name="height" placeholder="גובה" />
-        <input type="number" name="weight" placeholder="משקל" />
-        <select name="gender">
+        <input
+          {...register("height")}
+          type="number"
+          name="height"
+          placeholder="גובה"
+        />
+        <input
+          {...register("weight")}
+          type="number"
+          name="weight"
+          placeholder="משקל"
+        />
+        <select {...register("gender")} name="gender">
           <option value="male">זכר</option>
           <option value="female">נקבה</option>
           <option value="other">אחר</option>
         </select>
-        <select name="diabetesType">
+        <select {...register("diabetesType")} name="diabetesType">
           <option value="1">1</option>
           <option value="2">2</option>
         </select>
-        <input type="text" name="hmo" placeholder="קופת חולים" />
-        <div className="switch">
-             <input type="radio" className="switch-input" name="sugar" value="mgdl" id="mgdl"/>
-             <label htmlFor="mgdl" className="switch-label switch-label-off">mg/dl</label>
-              <input type="radio" className="switch-input" name="sugar" value="american" id="american"/>
-             <label htmlFor="american" className="switch-label switch-label-on">american units</label>
-             <span className="switch-selection"></span>
+        <input
+          {...register("hmo")}
+          type="text"
+          name="hmo"
+          placeholder="קופת חולים"
+        />
+        <div {...register("sugar")} className="switch">
+          <input
+            type="radio"
+            className="switch-input"
+            name="sugar"
+            value="mgdl"
+            id="mgdl"
+          />
+          <label htmlFor="mgdl" className="switch-label switch-label-off">
+            mg/dl
+          </label>
+          <input
+            type="radio"
+            className="switch-input"
+            name="sugar"
+            value="american"
+            id="american"
+          />
+          <label htmlFor="american" className="switch-label switch-label-on">
+            american units
+          </label>
+          <span className="switch-selection"></span>
         </div>
-        <div className="switch-count">
-             <input type="radio" className="switch-count-input" name="units" value="gram" id="gram"/>
-             <label htmlFor="gram" className="switch-count-label switch-count-label-off">גרם</label>
-              <input type="radio" className="switch-count-input" name="units" value="unit" id="unit"/>
-             <label htmlFor="unit" className="switch-count-label switch-count-label-on">מנות</label>
-             <span className="switch-count-selection"></span>
+        <div {...register("units")} className="switch-count">
+          <input
+            type="radio"
+            className="switch-count-input"
+            name="units"
+            value="gram"
+            id="gram"
+          />
+          <label
+            htmlFor="gram"
+            className="switch-count-label switch-count-label-off"
+          >
+        גרם
+          </label>
+          <input
+            type="radio"
+            className="switch-count-input"
+            name="units"
+            value="portion"
+            id="portion"
+          />
+          <label
+            htmlFor="unit"
+            className="switch-count-label switch-count-label-on"
+          >
+            מנות
+          </label>
+          <span className="switch-count-selection"></span>
         </div>
-        <div className="switch-protein">
-             <input type="radio" className="switch-protein-input" name="protein" value="include" id="include"/>
-             <label htmlFor="include" className="switch-protein-label switch-protein-label-off">כולל</label>
-              <input type="radio" className="switch-protein-input" name="protein" value="notInclude" id="notInclude"/>
-             <label htmlFor="notInclude" className="switch-protein-label switch-protein-label-on">לא כולל</label>
-             <span className="switch-protein-selection"></span>
+        <div {...register("protein")} className="switch-protein">
+          <input
+            type="radio"
+            className="switch-protein-input"
+            name="protein"
+            value="include"
+            id="include"
+          />
+          <label
+            htmlFor="include"
+            className="switch-protein-label switch-protein-label-off"
+          >
+            כולל
+          </label>
+          <input
+            type="radio"
+            className="switch-protein-input"
+            name="protein"
+            value="notInclude"
+            id="notInclude"
+          />
+          <label
+            htmlFor="notInclude"
+            className="switch-protein-label switch-protein-label-on"
+          >
+            לא כולל
+          </label>
+          <span className="switch-protein-selection"></span>
         </div>
         <div className="balance">
-            <input type="number" name="balance_min"/>
-            <input type="number" name="balance_max"/>
+          <input
+            {...register("balance_min")}
+            type="number"
+            name="balance_min"
+          />
+          <input
+            {...register("balance_max")}
+            type="number"
+            name="balance_max"
+          />
         </div>
+        <button type="submit">check</button>
       </form>
     </div>
   );
