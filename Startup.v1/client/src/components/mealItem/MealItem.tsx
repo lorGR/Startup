9d24ba544eebb5ad61ed6b08667ba12735640dcf
lioGR <liorgrishinnew@gmail.com>
2022-12-11 18:50:34 +0200

    added logic to dropdown

--- Startup.v1/client/src/components/mealItem/MealItem.tsx
+++ Startup.v1/client/src/components/mealItem/MealItem.tsx
@@ -10,10 +10,10 @@ interface MealItemProps {
 const MealItem: React.FC<MealItemProps> = ({meal}) => {
 
     const [mealServings, setMealServings] = useState<Food[]>([]);
+    const [dropDown, setDropDown] = useState<boolean>(false); 
 
     const handleClickMeal = async (event: any) => {
         try {
-            event.stopPropagation();
             let mealId = null;
             if(event.target.nodeName === 'P') {
                 mealId = event.target.parentNode.id;
@@ -23,30 +23,35 @@ const MealItem: React.FC<MealItemProps> = ({meal}) => {
             const { data } = await axios.post("/api/meals/get-meals-servings", {mealId});
             if(!data) throw new Error("Coudln't receive data from axios POST ON FUNCTION handleClickMeal IN FILE MealItem.tsx ");
             const { result } = data;
-            console.log(result);
 
-            console.dir(event.target);
-
+            if(!dropDown) {
+                setDropDown(!dropDown);
+            } else {
+                setDropDown(!dropDown)
+            }
             setMealServings(result);
-            // TODO: create dropdown menu when click on meal and render the mealServings
         } catch (error) {
             console.error(error);
         }
     }
 
     return (
-        <div onClick={handleClickMeal} className="meal-item dropbtn" id={meal.meal_id.toString()}>
-            <p>אינס׳ {meal.insulin}</p>
-            <p> פחמ׳ {meal.carbs}</p>
-            <p>{meal.time.slice(0, 5)}</p>
-            <div className="dropdown-content">
-                {mealServings.length > 0 && mealServings.map(mealServ => {
-                    return (
-                        <div className="meal-item__serving-item" key={mealServ.food_id}>
-                            {mealServ.food_name}
-                        </div>
-                    );
-                })}
+        <div className="meal-item dropbtn" >
+            <div onClick={handleClickMeal} className="meal-item__content" id={meal.meal_id.toString()}>
+                <p>אינס׳ {meal.insulin}</p>
+                <p> פחמ׳ {meal.carbs}</p>
+                <p>{meal.time.slice(0, 5)}</p>
+            </div>
+            <div className="dropdown-content ">
+                {
+                    dropDown && mealServings.length > 0 && mealServings.map(mealServ => {
+                        return (
+                            <div className="meal-item__serving-item" key={mealServ.food_id}>
+                                {mealServ.food_name}
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
     )
