16a6bf9d6cf3cd42cb390e7779c4ead831f9615f
lioGR <liorgrishinnew@gmail.com>
2022-12-13 13:19:39 +0200

    added render to meals at calendar

--- Startup.v1/client/src/views/calendar/Calendar.tsx
+++ Startup.v1/client/src/views/calendar/Calendar.tsx
@@ -4,16 +4,21 @@ import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import { getCurrentDate } from "../../helpers/helpers";
 import moment from "moment";
+import { Meal } from "../home/Home";
+import MealItem from "../../components/mealItem/MealItem";
 
 const Calendar = () => {
 
     const [date , setDate] = useState<any>(moment().format().slice(0,10));
 
+    const [meals, setMeals] = useState<Meal[]>([]);
+
     const getMealsByDate =  async () => {
         try {
             const { data } = await axios.post("/api/meals/get-meals-by-date", {date});
             if(!data) throw new Error("Couldn't receive date from axios POST 'get-meals-by-date' ");
-            console.log(data);
+            const { result } = data;
+            setMeals(result); 
         } catch (error) {
             console.error(error);
         }
@@ -48,6 +53,9 @@ const Calendar = () => {
                 <button onClick={handleDayFoward}>&lt;</button>
                 <span>{date}</span>
                 <button onClick={handleDayBack}>&gt;</button>
+            </div>
+            <div className="calendar__container">
+                {meals.map(meal => <MealItem meal={meal} key={meal.meal_id} setMeals={setMeals}/>)}
             </div>
         </div>
     )
