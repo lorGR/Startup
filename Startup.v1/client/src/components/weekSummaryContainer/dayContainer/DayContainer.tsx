b5ae4b695d25409bdd4d02b1167011d00e1beb31
lioGR <liorgrishinnew@gmail.com>
2022-12-22 14:30:45 +0200

    removed TODO

--- Startup.v1/client/src/components/weekSummaryContainer/dayContainer/DayContainer.tsx
+++ Startup.v1/client/src/components/weekSummaryContainer/dayContainer/DayContainer.tsx
@@ -29,7 +29,6 @@ const DayContainer: React.FC<DayContainerProps> = ({ displayDay, dayValue, maxVa
             <div className="bar-container">
                 <div style={{ height: barHeight }} className="bar">
                     <p>
-                        {/* //TODO: find what causes the bar not we in pracenteges height */}
                         {dayValue === 0 || dayValue === null ? "" : Math.round(dayValue!)}
                     </p>
                 </div>
