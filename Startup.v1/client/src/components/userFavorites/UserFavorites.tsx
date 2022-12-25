8212b79585363aa377e8fa7eeeb22692db7b3850
lioGR <liorgrishinnew@gmail.com>
2022-12-25 19:51:12 +0200

    implemented search food in favorites pages

--- Startup.v1/client/src/components/userFavorites/UserFavorites.tsx
+++ Startup.v1/client/src/components/userFavorites/UserFavorites.tsx
@@ -1,24 +1,37 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { FoodListItem } from './../foodList/foodListItem/FoodListItem';
 import { useAppDispatch, useAppSelector } from './../../app/hooks';
 import { favoriteFoodarraySelector } from './../../features/favoriteFood/favoriteFoodArraySlice';
-import { getAllUserFavoriteFood } from './../../features/favoriteFood/favoriteFoodArrayAPI';
+import { getAllUserFavoriteFood, getUserFavoritesFoodBySearch } from './../../features/favoriteFood/favoriteFoodArrayAPI';
 import { userSelector } from '../../features/user/userSlice';
 
 export const UserFavorites = () => {
-    const dispatch = useAppDispatch();
-    const favoriteFoodArray = useAppSelector(favoriteFoodarraySelector);
-    const user = useAppSelector(userSelector);
+  const dispatch = useAppDispatch();
+  const favoriteFoodArray = useAppSelector(favoriteFoodarraySelector);
+  const user = useAppSelector(userSelector);
+  const [userSearch, setUserSearch] = useState<string>("");
 
-    useEffect(() => {
-        dispatch(getAllUserFavoriteFood())
-    },[])
+  useEffect(() => {
+    if (userSearch.length <= 0) {
+      dispatch(getAllUserFavoriteFood());
+    } else {
+      dispatch(getUserFavoritesFoodBySearch({ userSearch }));
+    }
+  }, [userSearch]);
 
   return (
-    <div dir='rtl'>
-        {favoriteFoodArray?.map(food => {return (<FoodListItem key={food.food_id} foodItem={food} foodFavoritesArray={favoriteFoodArray} unit={user?.carbs_unit!}/>)})}
+    <div dir='rtl' className='foodList-favorites'>
+      <input
+        onChange={(e) => setUserSearch(e.target.value)}
+        dir="rtl"
+        type="text"
+        name="searchFood"
+        id="searchFood"
+        placeholder="חפש"
+      />
+      {favoriteFoodArray?.map(food => { return (<FoodListItem key={food.food_id} foodItem={food} foodFavoritesArray={favoriteFoodArray} unit={user?.carbs_unit!} />) })}
     </div>
-  )
+  );
 }
--- Startup.v1/server/API/userFavorites/userFavoriteRoutes.ts
+++ Startup.v1/server/API/userFavorites/userFavoriteRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUserFavorites, addToFavorites, deleteFromFavorite } from "./userFavoritesCtrl";
+import { getUserFavorites, addToFavorites, deleteFromFavorite, getUserFavirotesFoodBySearch } from "./userFavoritesCtrl";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router
     .get("/get-user-favorites", getUserFavorites)
     .post("/add-to-favorites", addToFavorites)
     .post("/delete-from-favorites", deleteFromFavorite)
+    .post("/get-user-favorites-by=search", getUserFavirotesFoodBySearch)
 
 export default router;
